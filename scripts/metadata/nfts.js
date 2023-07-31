83f478e372b2077d3f82aa6cf1971c5a3e00f3af
singhKalpana <127042824+kaapaS@users.noreply.github.com>
2023-08-01 01:27:52 +0530

    nfts.js

---
+++ scripts/metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "naruto",
+    image: "QmXTMXofRVRyQoNc1CubaWXEuJMYoAjUgtjqxcbe8Fwrfv",
+  },
+  {
+    name: "1",
+    description: "nanami",
+    image: "QmdFQWx8RgWMgTSioNSDP3QuMFSjLeaX4qahWPMUVTr3PR",
+  }, //change
+  {
+    name: "2",
+    description: "zoro",
+    image: "QmXF7nCyNj161LP25o3GtF8iPQ6WhLNMyRNixrywoogLE7",
+  },
+  {
+    name: "3",
+    description: "gojo",
+    image: "QmVhDbJSJvDyaGek6FkZjMKn1RTd7YaDdKL4b1ZC9GbZNn",
+  },
+  {
+    name: "4",
+    description: "demon slayer",
+    image: "QmYxZz72HcVuhDxndYPNcSJtWQ4EvaghAoUjLvLb77UmnQ",
+  },
+];
+
+module.exports = nfts;
